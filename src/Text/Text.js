496d9b4a7472fb1bfbab62f7c0ef9692b4493979
Damiano Carradori <damiano.carradori@gmail.com>
2019-01-21 22:14:54 +0100

    Added attributes

--- src/Text/Text.js
+++ src/Text/Text.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react'
-import {FormContext} from '../Form'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { FormContext } from '../Form'
 
 class Text extends Component {
     constructor(props) {
@@ -7,25 +8,68 @@ class Text extends Component {
     }
 
     componentDidMount() {
-        const {name} = this.props;
-        const {onMount} = this.context;
+        const { name } = this.props;
+        const { onMount } = this.context;
 
         onMount({
             name,
             type: 'text',
-
         });
     }
 
     render() {
-        const {name, placeholder} = this.props;
-        const {onChange} = this.context;
+        const {
+            name,
+            defaultValue,
+            readOnly,
+            disabled,
+            size,
+            maxLength,
+            autoComplete,
+            autoFocus,
+            pattern,
+            placeholder,
+            required,
+        } = this.props;
+
+        const { onChange } = this.context;
         return (
-            <input type="text" name={name} placeholder={placeholder} onChange={onChange}/>
+            <input
+                type="text"
+                onChange={onChange}
+
+                name={name}
+                defaultValue={defaultValue}
+                readOnly={readOnly}
+                disabled={disabled}
+                size={size}
+                maxLength={maxLength}
+                autoComplete={autoComplete}
+                autoFocus={autoFocus}
+                pattern={pattern}
+                placeholder={placeholder}
+                required={required}
+            />
         );
     }
 }
 
 Text.contextType = FormContext;
+
+Text.propTypes = {
+    name: PropTypes.string.isRequired,
+    defaultValue: PropTypes.string,
+    readOnly: PropTypes.bool,
+    disabled: PropTypes.bool,
+    size: PropTypes.number,
+    maxLength: PropTypes.number,
+    autoComplete: PropTypes.oneOf(['on', 'off']),
+    autoFocus: PropTypes.bool,
+    pattern: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+};
+
+Text.defaultProps = {};
 
 export default Text
