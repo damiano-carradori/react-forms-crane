c89c8527c7a1daac234c44dac07fcfb84c85150d
Damiano Carradori <damiano.carradori@gmail.com>
2019-01-27 11:06:04 +0100

    Added custom value

--- src/Reset/Reset.js
+++ src/Reset/Reset.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 class Reset extends Component {
     render() {
         const {
+            value,
             disabled,
             autoFocus,
         } = this.props;
@@ -13,6 +14,7 @@ class Reset extends Component {
                 <input
                     type="reset"
 
+                    value={value}
                     disabled={disabled}
                     autoFocus={autoFocus}
                 />
