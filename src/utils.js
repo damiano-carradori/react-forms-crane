b0d909fa53404728ae37ab596e8689d7da91f834
Damiano Carradori <damiano.carradori@gmail.com>
2019-02-03 13:29:57 +0100

    Added a map function for objects

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,9 @@ export const kebab_case = string =>
         .replace(/([a-z])([A-Z])/g, '$1-$2')
         .replace(/\s+/g, '-')
         .toLowerCase();
+
+export const mapObject = (object, fn) =>
+    Object.keys(object).reduce((result, key) => {
+        result[key] = fn(object[key]);
+        return result
+    }, {});
