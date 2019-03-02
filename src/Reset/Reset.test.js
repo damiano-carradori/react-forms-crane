8d91ce35e9d6c0609501b7b590e25c774e9cf186
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-02 14:31:24 +0100

    Wrote tests for Reset

---
+++ src/Reset/Reset.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import Reset from './Reset'
+
+describe('Reset', () => {
+
+    it('should create correctly the input with the right props', function () {
+        const value = 'Reset value';
+
+        const wrapper = shallow(<Reset value={value} />);
+
+        const i = wrapper.find('input');
+
+        expect(i.prop('type')).toBe('reset');
+        expect(i.prop('value')).toBe(value);
+    });
+
+});
