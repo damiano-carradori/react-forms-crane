393916bb02db2a059f83d15d519706d2ced5cea3
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-09 03:04:20 +0100

    Created Password story

---
+++ src/Password/story.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Password from './Password';
+import { FormContextProvider } from '../Form/FormContext';
+
+storiesOf('Password', module)
+    .addDecorator(storyFn => <FormContextProvider onSubmit={console.log}>{storyFn()}</FormContextProvider>)
+    .add('default', () => (
+        <Password name="pass" value="test"/>
+    ));
