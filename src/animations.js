308b9992bb2c67501ae2824f746cd2440bd1fe6f
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-09 14:56:39 +0100

    added animations

---
+++ src/animations.js
@@ -0,0 +1,22 @@
+import { keyframes } from 'styled-components';
+
+export const shake = keyframes`
+  from,
+  to {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  16.67%,
+  50%,
+  83.33% {
+    -webkit-transform: translate3d(-10px, 0, 0);
+    transform: translate3d(-10px, 0, 0);
+  }
+
+  33%,
+  66.67% {
+    -webkit-transform: translate3d(10px, 0, 0);
+    transform: translate3d(10px, 0, 0);
+  }
+`;
