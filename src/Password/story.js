29aa2186a1e12652ca1e20c7b0b1304b81f71565
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-11 00:30:46 +0100

    added placeholder

--- src/Password/story.js
+++ src/Password/story.js
@@ -6,7 +6,7 @@ import { FormContextProvider } from '../Form/FormContext';
 storiesOf('Password', module)
     .addDecorator(storyFn => <FormContextProvider onSubmit={console.log}>{storyFn()}</FormContextProvider>)
     .add('default', () => (
-        <Password name="pass" />
+        <Password name="pass" placeholder="Write your password here..." />
     ))
     .add('disabled', () => (
         <Password name="pass" defaultValue="test" disabled />
