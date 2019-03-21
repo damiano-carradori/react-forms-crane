3714f4bddad926ece5da0d46543cf54fb5948d68
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-22 00:27:36 +0100

    styled Select component

--- src/Select/story.js
+++ src/Select/story.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Select from './Select';
-import { FormContextProvider } from '../Form/FormContext';
+import Form from '../Form';
+import Submit from '../Submit';
 
 storiesOf('Select', module)
-    .addDecorator(storyFn => <FormContextProvider onSubmit={console.log}>{storyFn()}</FormContextProvider>)
-    .add('with text', () => (
-        <Select name="test" options={['option1','option2']}/>
+    .addDecorator(storyFn => <Form onSubmit={console.log}>{storyFn()}</Form>)
+    .add('default', () => (
+        <Select name="test" options={['option1', 'option2']}/>
+    ))
+    .add('multiple', () => (
+        <Select name="test" options={['option1', 'option2']} multiple/>
+    ))
+    .add('disabled', () => (
+        <Select name="test" options={['option1', 'option2']} disabled/>
+    ))
+    .add('test', () => (
+        <React.Fragment>
+            <Select name="test" options={[{ value: 'option1', label: 'Option 1' }, { value: 'option2', label: 'Option 2' }]} multiple/>
+            <Select name="test2" options={['option1', 'option2']}/>
+            <Submit/>
+        </React.Fragment>
     ));
