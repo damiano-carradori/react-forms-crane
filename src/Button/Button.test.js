e6231272afcf2cff3d7ed7c358e5099119acd8ee
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-22 14:58:10 +0100

    fixed Button tests

--- src/Button/Button.test.js
+++ src/Button/Button.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Button from './Button'
 
 describe('Button', () => {
@@ -7,7 +7,7 @@ describe('Button', () => {
     it('should create the input and set props correctly', function () {
         const value = 'Button value';
         const onClick = jest.fn();
-        const wrapper = shallow(<Button onClick={onClick} value={value} disabled/>);
+        const wrapper = mount(<Button onClick={onClick} value={value} disabled/>);
 
         expect(wrapper.find('input').prop('type')).toBe('button');
         expect(wrapper.find('input').prop('value')).toBe(value);
