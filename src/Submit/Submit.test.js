90c88f67638f482f849b6fff845b748846c98f6f
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-22 15:25:56 +0100

    fixed Submit tests

--- src/Submit/Submit.test.js
+++ src/Submit/Submit.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Submit from './Submit'
 
 describe('Submit', () => {
@@ -7,7 +7,7 @@ describe('Submit', () => {
     it('should create correctly the input with the right props', function () {
         const value = 'Submit value';
 
-        const wrapper = shallow(<Submit value={value}/>);
+        const wrapper = mount(<Submit value={value}/>);
 
         const i = wrapper.find('input');
 
