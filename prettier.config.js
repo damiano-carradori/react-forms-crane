d7b1dd94236451aa2cfcf0f17ddb014fd98d695d
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-22 16:14:03 +0100

    added prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    overrides: [
+        {
+            files: "*.json",
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
