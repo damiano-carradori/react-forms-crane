7d770a6627164f934f8a7f56923150287dee264e
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-29 15:09:56 +0100

    added fields exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+import Text from "./Text";
+import Checkbox from "./Checkbox";
+import RadioButton from "./RadioButton";
+import Password from "./Password";
+import Select from "./Select";
+import TextArea from "./TextArea";
+import File from "./File";
+import Button from "./Button";
+import Submit from "./Submit";
+import Reset from "./Reset";
+
 export { default } from "./Form";
+
+export {
+    Text,
+    Checkbox,
+    RadioButton,
+    Password,
+    Select,
+    TextArea,
+    File,
+    Button,
+    Submit,
+    Reset,
+};
