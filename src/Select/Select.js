3030fb3460431de794be25f7d11e37d1432e96e9
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-29 16:21:41 +0100

    enhanced, added style and tests

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -1,6 +1,16 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import { FormContext } from "../Form";
+import { CSSTransition } from "react-transition-group";
+import { FormContext } from "../FormContext";
+import {
+    HiddenSelect,
+    OptionsWrapper,
+    SelectPlaceholder,
+    StyledOption,
+    StyledSelect,
+    Wrapper,
+} from "./style";
+import { filterObjectKeys } from "../utils";
 
 function Select({
     name,
@@ -12,6 +22,9 @@ function Select({
     required,
     disabled,
 }) {
+    const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState({});
+
     const { onMount, onChange } = useContext(FormContext);
 
     const elemRef = useRef();
@@ -26,7 +39,56 @@ function Select({
         }
     }, [elemRef]);
 
-    const renderedOptions = options.map((option, index) => {
+    useEffect(() => {
+        if (multiple) {
+            const optionsToSelect = Object.values(selected);
+            [...elemRef.current.options]
+                .map(o => o.value)
+                .map((option, i) => {
+                    elemRef.current.options[
+                        i
+                    ].selected = optionsToSelect.includes(option);
+                });
+        } else {
+            elemRef.current.value = selected.value;
+        }
+        const event = document.createEvent("HTMLEvents");
+        event.initEvent("change", true, false);
+        elemRef.current.dispatchEvent(event);
+    }, [selected]);
+
+    const toggleOptions = () => {
+        if (!disabled) {
+            setOpen(!open);
+        }
+    };
+
+    const selectOption = option => {
+        let value, label;
+        if ("string" === typeof option) {
+            value = option;
+            label = option;
+        } else {
+            value = option.value;
+            label = option.label;
+        }
+
+        if (multiple) {
+            if (selected[label]) {
+                setSelected(filterObjectKeys(selected, key => key !== label));
+            } else {
+                setSelected({
+                    ...selected,
+                    [label]: value,
+                });
+            }
+        } else {
+            setSelected({ value, label });
+            setOpen(false);
+        }
+    };
+
+    const renderOption = (option, index) => {
         let value, label;
         if ("string" === typeof option) {
             value = option;
@@ -40,11 +102,39 @@ function Select({
                 {label}
             </option>
         );
-    });
+    };
+
+    const renderStyledOption = (option, index) => {
+        let value, label;
+        if ("string" === typeof option) {
+            value = option;
+            label = option;
+        } else {
+            value = option.value;
+            label = option.label;
+        }
+        return (
+            <StyledOption
+                key={index}
+                onClick={() => selectOption(option)}
+                multiple={multiple}
+                selected={multiple ? selected[label] : selected.value === value}
+            >
+                {label}
+            </StyledOption>
+        );
+    };
+
+    const renderedOptions = options.map(renderOption);
+    const styledOptions = options.map(renderStyledOption);
+
+    const placeholder = multiple
+        ? Object.keys(selected).join(", ") || "Select..."
+        : (selected && selected.label) || "Select...";
 
     return (
-        <div>
-            <select
+        <Wrapper>
+            <HiddenSelect
                 ref={elemRef}
                 onChange={onChange}
                 name={name}
@@ -56,8 +146,22 @@ function Select({
                 disabled={disabled}
             >
                 {renderedOptions}
-            </select>
-        </div>
+            </HiddenSelect>
+            <StyledSelect>
+                <SelectPlaceholder disabled={disabled} onClick={toggleOptions}>
+                    {placeholder}
+                </SelectPlaceholder>
+                <CSSTransition
+                    in={!disabled && open}
+                    timeout={300}
+                    classNames="select-options"
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    <OptionsWrapper>{styledOptions}</OptionsWrapper>
+                </CSSTransition>
+            </StyledSelect>
+        </Wrapper>
     );
 }
 
