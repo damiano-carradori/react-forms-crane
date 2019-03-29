f101d66561ed0f6dcf5bfae747431b8be1834ad9
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-29 16:24:57 +0100

    prettified and changed externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    mode: "development",
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2',
+        path: path.resolve(__dirname, "build"),
+        filename: "index.js",
+        libraryTarget: "commonjs2",
     },
-    devtool: 'cheap-module-source-map',
+    devtool: "cheap-module-source-map",
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                include: [
-                    path.resolve(__dirname, 'src'),
-                ],
-                exclude: [
-                    path.resolve(__dirname, 'node_modules'),
-                ],
-                loader: 'babel-loader',
+                include: [path.resolve(__dirname, "src")],
+                exclude: [path.resolve(__dirname, "node_modules")],
+                loader: "babel-loader",
             },
         ],
     },
     externals: {
-        react: {
-            root: 'React',
-            commonjs2: 'react',
-            commonjs: 'react',
-            amd: 'react',
-        },
+        react: "commonjs react",
     },
 };
