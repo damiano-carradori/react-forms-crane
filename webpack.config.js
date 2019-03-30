c110687729cb9c344d7bc2cc191b9684ef8a3a83
Damiano Carradori <damiano.carradori@gmail.com>
2019-03-30 11:28:30 +0100

    updated externals and added react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
         ],
     },
     externals: {
-        react: "commonjs react",
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+        },
+        "react-dom": {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+        },
     },
 };
